b6245ede0dd572051202c85aaf5829ed85aa1cc8
Albert Lu <albertlu@utexas.edu>
2025-01-12 21:00:07 -0600

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const config = {
     mongodb: {
-        uri: process.env.MONGODB_URI || 'mongodb://albertlu:test@ac-6qvt5dn-shard-00-00.nr8sj.mongodb.net:27017,ac-6qvt5dn-shard-00-01.nr8sj.mongodb.net:27017,ac-6qvt5dn-shard-00-02.nr8sj.mongodb.net:27017/profile_matching?ssl=true&replicaSet=atlas-yzjm6q-shard-0&authSource=admin'
+        uri: process.env.MONGODB_URI || 'mongodb+srv://albertlu:test@aphrodite.nr8sj.mongodb.net/profile_matching?retryWrites=true&w=majority&appName=Aphrodite&tls=true'
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'aphrodite_secret_key_2025'
