a233853515818b642d3e95a31918b46365a12fb9
Albert Lu <albertlu@utexas.edu>
2025-01-13 13:42:33 -0600

    undo

--- frontend/src/apiClient.js
+++ frontend/src/apiClient.js
@@ -2,18 +2,15 @@ import config from './config';
 
 const fetchWithSSLBypass = async (endpoint, options = {}) => {
     const url = `${config.apiUrl}${endpoint}`;
-    const fetchOptions = {
-        ...options,
-        headers: {
-            'Content-Type': 'application/json',
-            ...options.headers,
-        },
-        mode: 'cors',
-        credentials: 'include'
-    };
-
     try {
-        const response = await fetch(url, fetchOptions);
+        const response = await fetch(url, {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json',
+                ...options.headers,
+            },
+            mode: 'cors'
+        });
 
         if (!response.ok) {
             const data = await response.json();
